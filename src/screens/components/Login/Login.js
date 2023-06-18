5898a51603b0a687ab56b8797255f4df09569c33
NAS-Ragde <117576565+NAS-Ragde@users.noreply.github.com>
2023-06-18 14:33:02 +0200

    Fix style in Login.css Add Sign in button

--- src/screens/components/Login/Login.js
+++ src/screens/components/Login/Login.js
@@ -4,15 +4,23 @@ function Login() {
     return (
         <div className="background">
             <div className="loginWrapper">
-                <h1 className={"text"}> Fundamentals of Medical Imaging Management </h1>
-                <input placeholder="User" className="textInput" />
-                <input placeholder="Password" type="password" className="textInput" />
-                <button
-                    className="button"
+                <h1 className="header"> Fundamentals of Medical Imaging Management </h1>
+                <input placeholder="User" className="textInput"/>
+                <input placeholder="Password" type="password" className="textInput"/>
+                <span
+                    className="login"
                     //onClick={() => navigation.navigate('Home')}
                 >
                     Login
-                </button>
+                </span>
+                <p className={"text"}> Dont you have an account? {
+                    <span
+                    className="sign-in"
+                    //onClick={() => navigation.navigate('Home')}
+                >
+                    Sign in
+                </span>} </p>
+
             </div>
         </div>
     );
