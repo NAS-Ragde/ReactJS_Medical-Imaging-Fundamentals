279442bebb7684d6ed3b63c0c1e55c4873861c1e
NAS-Ragde <117576565+NAS-Ragde@users.noreply.github.com>
2023-06-21 20:25:01 +0200

    Progress: -Registration handle Submit

--- src/screens/components/Registration/Registration.js
+++ src/screens/components/Registration/Registration.js
@@ -1,46 +1,91 @@
 import React, {useState} from "react";
 import './Registration.css'
+import {useForm} from "react-hook-form";
 export default function Registration () {
 
-    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    let [isValidated] = useState('false');
+
+    const {
+        control,
+        register,
+        handleSubmit,
+        formState: {isSubmitted, errors},
+    } = useForm({
+        mode: 'all',
+        defaultValues:{
+            email: '',
+            password: '',
+            confirmPassword:'',
+        }
+    })
+
+    const passwordCharacters = (value) => {
+        const characters = new RegExp( /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*['!"#$%&'()*+,/\-.:;<=>?@[\]^_`{|}~']).{8,}$/);
+        return  isValidated = characters.test(value) ? isValidated : errors
+    }
+
+    const passwordValidation = (value) => {
+        return console.log('process incomplete', value)
+    }
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
 
     return(
         <div className="background">
             <div className="loginWrapper">
                 <h1 className="header"> Registration </h1>
+
                 <input
-                    placeholder="Name"
-                    className="textInput"
-                    onChange={()=> setUsername()}
-                    value={username}
-                />
-                <input
+                    {...register("email", {required: true}) }
+                    autoCapitalize={'off'}
                     placeholder="Email"
                     className="textInput"
-                    onChange={()=>{setEmail()} }
-                    value={email}
+                    maxLength={50}
                 />
+
                 <input
+                    {...register("password", {required: true}) }
                     placeholder="Password"
                     type="password"
                     className="textInput"
-                    onChange={()=> setPassword()}
-                    value={password}
+                    autoComplete={'false'}
                 />
+
                 <input
+                    {...register("confirmPassword", {required: true}) }
                     placeholder="Confirm Password"
                     type="password"
                     className="textInput"
-                    onChange={()=> setConfirmPassword()}
-                    value={confirmPassword}
+                    autoComplete={'false'}
                 />
 
-                <span className={'submission'}>Submit</span>
+                <span className={'submission'} onSubmit={handleSubmit(onSubmit)}>Submit</span>
+
 
             </div>
         </div>
     );
 }
+
+
+//
+//     ()=> {
+//         passwordCharacters(password)
+//             ? setPassword(password)}
+//             : console.log('error in characters')}
+//   }
+
+
+//
+//    ()=> {
+//        passwordValidation(confirmPassword)}
+//       }
+//
+
+
+
