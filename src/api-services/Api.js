8c5b2ea51fc19d8a92025c400a2d1c267531de2d
NAS-Ragde <117576565+NAS-Ragde@users.noreply.github.com>
2023-06-22 18:13:45 +0200

    Registration initial test backend

--- src/api-services/Api.js
+++ src/api-services/Api.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 export const baseUrl = 'http://localhost:8080';
-export const STUDENTS_REGISTRATION = 'http://localhost:8080/students';
+export const STUDENTS_REGISTRATION = 'http://localhost:8080/students/register';
 export const CHAPTERS = 'http://localhost:8080/chapters';
 export const QUIZ = 'http://localhost:8080/quizes';
 
+export const auth_UUID = '4784cb09-a9b6-4003-8861-daf2d54d1718';
+export const auth_USER = 'nayeli'
 
--- src/screens/components/Registration/Registration.js
+++ src/screens/components/Registration/Registration.js
@@ -1,12 +1,17 @@
 import React, {useState} from "react";
 import './Registration.css'
 import {useForm} from "react-hook-form";
+import axios from "axios";
+import {STUDENTS_REGISTRATION} from "../../../api-services/Api";
 export default function Registration () {
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    let [isValidated] = useState('false');
+     const [email, setEmail] = useState('');
+     const [password, setPassword] = useState('');
+     const [confirmPassword, setConfirmPassword] = useState('');
+     let [isValidated] = useState('false');
+
+    // localStorage.setItem('uuid', response.uuid);
+    // localStorage.setItem('username', response.username);
 
     const {
         control,
@@ -27,44 +32,67 @@ export default function Registration () {
         return  isValidated = characters.test(value) ? isValidated : errors
     }
 
-    const passwordValidation = (value) => {
+    const passwordValidation =  (value) => {
         return console.log('process incomplete', value)
     }
 
-    const onSubmit = (data) => {
-        console.log(data)
-    }
+    const onSubmit = async (data) => {
+        console.log(data);
+
+        if (Object.keys(data.length !== 0)) {
+
+            await axios
+                .post(STUDENTS_REGISTRATION + "?username=" + data.email + "&password=" + data.password)
+                .then ((response) => console.log('Posted:', response.data))
+                .catch((error) => {
+                    console.log(error);
+                })
+
+    }}
 
     return(
         <div className="background">
             <div className="loginWrapper">
                 <h1 className="headerRegistration"> Registration </h1>
 
-                <input
-                    {...register("email", {required: true}) }
-                    autoCapitalize={'off'}
-                    placeholder="Email"
-                    className="textInput"
-                    maxLength={50}
-                />
+                <div className="input-wrapper">
+                    <input
+                        {...register("email", {required: true}) }
+                        autoCapitalize={'off'}
+                        placeholder="Email"
+                        type="email"
+                        className="input-registration"
+                        maxLength={50}
+                    />
+                </div>
 
-                <input
-                    {...register("password", {required: true}) }
-                    placeholder="Password"
-                    type="password"
-                    className="textInput"
-                    autoComplete={'false'}
-                />
+                <div className="input-wrapper">
+                    <input
+                        {...register("password", {required: true}) }
+                        placeholder="Password"
+                        type="password"
+                        className="input-registration"
+                        autoComplete={'false'}
+                    />
+                </div>
 
-                <input
-                    {...register("confirmPassword", {required: true}) }
-                    placeholder="Confirm Password"
-                    type="password"
-                    className="textInput"
-                    autoComplete={'false'}
-                />
+                <div className="input-wrapper">
+                    <input
+                        {...register("confirmPassword", {required: true}) }
+                        placeholder="Confirm Password"
+                        type="password"
+                        className="input-registration"
+                        autoComplete={'false'}
+                    />
+                    { }
+                </div>
 
-                <span className={'submission'} onSubmit={handleSubmit(onSubmit)}>Submit</span>
+               <form className={'submission'} onSubmit={handleSubmit(onSubmit)}>
+                   <input
+                       type ={'Submit'}
+                       className={'submission'}
+                   />
+               </form>
 
 
             </div>
