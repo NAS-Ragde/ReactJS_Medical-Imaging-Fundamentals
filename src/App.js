b6245281999a583c93adcd6f4fbfe94f5590fad5
NAS-Ragde <117576565+NAS-Ragde@users.noreply.github.com>
2023-06-22 22:19:55 +0200

    Router in App.js Comment Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import React from 'react';
-// import Login from "./screens/components/Login/Login";
+
 import Registration from "./screens/components/Registration/Registration";
+import Home from "./screens/components/Home/Home";
+import Login from "./screens/components/Login/Login";
+import Content from "./screens/components/Content/Content";
+import {BrowserRouter as Router, Routes, Route, Link, NavLink, Navigate} from 'react-router-dom';
 
 function App() {
-  return (
-    <Registration />
-  );
+
+    const isLoggedIn = localStorage.getItem('uuid');
+
+    return <Router>
+        <Routes>
+            <Route
+                path='/'
+                element={<Home />}/>
+            <Route
+                path='/Login'
+                element={<Login/>}
+            />
+            <Route
+                path='/Home'
+                element={<Home />}
+            />
+            <Route
+                path='/Registration'
+                element={<Registration />}
+            />
+            <Route
+                path='/Content'
+                element={<Content />}
+            />
+            {/*<Route*/}
+            {/*    path='/'*/}
+            {/*    element={isLoggedIn ? (<Home />) : (<Login/>)}/>*/}
+            {/*<Route*/}
+            {/*    path='/Login'*/}
+            {/*    element={isLoggedIn ? (<Home />) : (<Login/>)}*/}
+            {/*/>*/}
+            {/*<Route*/}
+            {/*    path='/Home'*/}
+            {/*    element={isLoggedIn ? (<Home />) : (<Login/>)}*/}
+            {/*/>*/}
+            {/*<Route*/}
+            {/*    path='/Registration'*/}
+            {/*    element={<Registration />}*/}
+            {/*/>*/}
+            {/*<Route*/}
+            {/*    path='/Content'*/}
+            {/*    element={isLoggedIn ? (<Content />) : (<Login/>)}*/}
+            {/*/>*/}
+        </Routes>
+    </Router>
 }
 export default App;
--- src/screens/components/Navigation/Navigation.js
+++ src/screens/components/Navigation/Navigation.js
@@ -1,30 +1,30 @@
-import * as React from "react";
-import {Route, Router, Routes} from "react-router-dom";
-import Home from "../Home/Home";
-import Login from "../Login/Login";
-import Registration from "../Registration/Registration";
-import Content from "../Content/Content";
-
-export default function Navigation() {
-    return (
-        <Router>
-                <Route
-                    exact path={'../Home'}
-                    element={<Home/>}
-                />
-                <Route
-                    exact path={'../Login'}
-                    element={<Login/>}
-                />
-                <Route
-                    exact path={'../Registration'}
-                    element={<Registration/>}
-                />
-                <Route
-                    exact path={'../Content'}
-                    element={<Content/>}
-                />
-        </Router>
-
-    );
-}
+// import * as React from "react";
+// import {Route, Router, Routes} from "react-router-dom";
+// import Home from "../Home/Home";
+// import Login from "../Login/Login";
+// import Registration from "../Registration/Registration";
+// import Content from "../Content/Content";
+//
+// export default function Navigation() {
+//     return (
+//         <Router>
+//                 <Route
+//                     exact path={'../Home'}
+//                     element={<Home/>}
+//                 />
+//                 <Route
+//                     exact path={'../Login'}
+//                     element={<Login/>}
+//                 />
+//                 <Route
+//                     exact path={'../Registration'}
+//                     element={<Registration/>}
+//                 />
+//                 <Route
+//                     exact path={'../Content'}
+//                     element={<Content/>}
+//                 />
+//         </Router>
+//
+//     );
+// }
