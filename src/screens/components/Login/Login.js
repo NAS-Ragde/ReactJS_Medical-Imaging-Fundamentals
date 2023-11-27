1af054faedfe534257b6c0c3fbd39e236e488372
ns04984 <nayeli.anaid.silva@gmail.com>
2023-11-27 10:46:19 +0100

    Add Error messages in Login.js and Login.css

--- src/screens/components/Login/Login.js
+++ src/screens/components/Login/Login.js
@@ -1,19 +1,17 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './Login.css';
 import {useNavigate} from "react-router-dom";
 import {useForm} from "react-hook-form";
 import axios from "axios";
 import {LOGIN, STORAGE_KEY} from "../../../api-services/Api";
-
 export default function Login() {
 
-    const [userEmail, setUserEmail] = useState ();
-    const [password, setPassword] = useState();
+    const [error, setError] = useState('');
 
     const {
         register,
         handleSubmit,
-        formState: {errors}
+        formState: {errors, isSubmitted }
     } = useForm({
         mode: 'all',
         defaultValues:{
@@ -21,6 +19,10 @@ export default function Login() {
             password: '',
         }
     })
+
+    useEffect(()=> {
+        errors? setError('Empty credentials are not valid for login.'):  setError('');
+    },[errors,setError])
 
     const navigate = useNavigate();
     const redirectUser = () => {
@@ -45,8 +47,7 @@ export default function Login() {
                     console.log(error);
                 })
 
-        }
-
+        } setError('Error in Login, please verify your account');
     }
 
     return (
@@ -58,6 +59,7 @@ export default function Login() {
                     {...register("email", {required: true}) }
                     placeholder="User"
                     className="textInput"
+                    max={50}
                 />
 
                 <input
@@ -65,21 +67,20 @@ export default function Login() {
                     placeholder="Password"
                     type="password"
                     className="textInput"
+                    max={30}
                 />
 
                 <form
                     className={'login'}
                 >
                     <span className={'login'}  onClick={handleSubmit(login)}>Login</span>
+                    {isSubmitted && errors && <p className={'error-login'}>{error}</p>}
                 </form>
 
                 <p className={"sign-in-text"}> Dont you have an account? {
                     <span className="sign-in" onClick={()=> navigate('/registration') } >Sign up </span>
                     }
                 </p>
-
-
-
             </div>
         </div>
     );
